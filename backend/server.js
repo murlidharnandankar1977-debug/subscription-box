a1826f93e704f22818052b89a62d5cf94549b707
shreya <murlidharnandankar1977@gmail.com>
2026-05-24 17:53:54 +0530

    atlas added

--- backend/server.js
+++ backend/server.js
@@ -13,7 +13,7 @@ app.use(cors());
 app.use(express.json());
 
 // MongoDB connect
-mongoose.connect("mongodb://127.0.0.1:27017/subscription_box")
+mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log("MongoDB Connected"))
   .catch((err) => console.log(err));
 
